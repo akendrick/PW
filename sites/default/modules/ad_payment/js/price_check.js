d5b0ce04d4800db025bf88f5a95379ccb582b846
kendrick <shrimphead@389843.no-reply.drupal.org>
2011-11-02 16:51:04 -0700

    Snapshot: jQuery Proof of Concept

--- sites/default/modules/ad_payment/js/price_check.js
+++ sites/default/modules/ad_payment/js/price_check.js
@@ -1,13 +1,24 @@
+Drupal.adPay = Drupal.adPay || {};
+
+Drupal.adPay.wordCounter = function() {
+  var adCopy;
+  adCopy = jQuery('#edit-field-ad-copy-und-0-value').val();
+
+  var wordCount;
+  wordCount = jQuery.trim(jQuery('#edit-field-ad-copy-und-0-value').val().split(/\s+/)).wordCount;
+
+  
+  message = Drupal.t("Word count: @count", {'@count': wordCount});
+  return message;
+}
+
 jQuery(document).ready(function() {
+  jQuery('#field-price-add-more-wrapper').hide();
+  jQuery('#field-ccid-add-more-wrapper').hide();
+
   // Default jQuery Counter script.
-//  Drupal.adP.init = function(id, settings) {
-    
-  //  var box;
-    
- // }
-
-
-
+  jQuery('#ad-price-box').html(Drupal.adPay.wordCounter());
 
 
 });
+
