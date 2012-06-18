bbf39891fda75d4ffe27c00ef3a7535007918f55
Kendrick <wallncrown@mac.com>
2012-06-18 13:12:50 -0700

    UPDATE - User Role Field, File Entity, Filter Permisions, Global Redirect, Real Name, Token, Media

--- sites/all/modules/media/js/media.admin.js
+++ sites/all/modules/media/js/media.admin.js
@@ -9,17 +9,38 @@
 (function ($) {
 
 /**
- * Functionality for the administrative file listings.
+ * Functionality for the thumbnail display
  */
 Drupal.behaviors.mediaAdmin = {
   attach: function (context) {
     // Show a javascript confirmation dialog if a user has files selected and
     // they try to switch between the "Thumbnail" and "List" local tasks.
-    $('.tabs.secondary a').bind('click', function () {
-      if ($(':checkbox:checked', $('.file-entity-admin-files-form')).length != 0) {
+    $('.media-display-switch a').bind('click', function () {
+      if ($(':checkbox:checked', $('form#media-admin')).length != 0) {
         return confirm(Drupal.t('If you switch views, you will lose your selection.'));
       }
     });
+
+    // Configure the "Add file" link to fire the media browser popup.
+    var $launcherLink = $('<a class="media-launcher" href="#"></a>').html(Drupal.t('Add file'));
+    $launcherLink.bind('click', function () {
+      // This option format needs *serious* work.
+      // Not even bothering documenting it because it needs to be thrown.
+      // See media.browser.js and media.browser.inc - media_browser()
+      // For how it gets passed.
+      var options = {
+        disabledPlugins: ['library'],
+        multiselect: true
+      };
+      Drupal.media.popups.mediaBrowser(function (mediaFiles) {
+        // When the media browser succeeds, we refresh
+        // @TODO: Should jump to the new media file and perhaps highlight it.
+        parent.window.location.reload();
+        return false;
+      }, options);
+    });
+
+    $('ul.action-links', context).prepend($('<li></li>').append($launcherLink));
 
     if ($('.media-display-thumbnails').length) {
       // Implements 'select all/none' for thumbnail view.
@@ -39,7 +60,7 @@ Drupal.behaviors.mediaAdmin = {
         .append(allLink)
         .append(', ')
         .append(noneLink)
-        .prependTo('.media-display-thumbnails')
+        .prependTo('#media-admin > div')
       // If the media item is clicked anywhere other than on the image itself
       // check the checkbox. For the record, JS thinks this is wonky.
       $('.media-item').bind('click', function (e) {
@@ -71,8 +92,29 @@ Drupal.behaviors.mediaAdmin = {
         });
       });
     }
+
+    // When any checkboxes are clicked on this form check to see if any are checked.
+    // If any checkboxes are checked, show the edit options (@todo rename to edit-actions).
+    $('#media-admin :checkbox').bind('change', function () {
+      Drupal.behaviors.mediaAdmin.showOrHideEditOptions();
+    });
+
+    Drupal.behaviors.mediaAdmin.showOrHideEditOptions();
+  },
+
+  // Checks if any checkboxes on the form are checked, if so it will show the
+  // edit-options panel.
+  showOrHideEditOptions: function() {
+    var fieldset = $('#edit-options');
+    if (!$('#media-admin input[type=checkbox]:checked').size()) {
+      fieldset.slideUp('fast');
+    }
+    else {
+      fieldset.slideDown('fast');
+    }
   }
 };
+
 
 /**
  * JavaScript for the Media types administrative form.
@@ -96,5 +138,7 @@ Drupal.behaviors.mediaTypesAdmin = {
     });
   }
 };
+
+
 
 })(jQuery);
--- sites/all/modules/media/media.api.php
+++ sites/all/modules/media/media.api.php
@@ -2,54 +2,91 @@
 
 /**
  * @file
- * Hooks provided by the Media module.
+ * Hook provided by the media module.
  */
+
+/**
+ * Return an array of plugins for the media browser.
+ *
+ * Implementors are expected to return a renderable element.
+ *
+ * Each element will be a jQuery tab on the media browser.
+ *
+ * Some elements are special:
+ *  - #title: The title that goes on the tab
+ *  - #settings: Drupal.settings.media.browser.$key (where key is the array key).
+ *  - #callback: If provided, will make the tab an "ajax" tab.
+ *
+ * Example:
+ *   $plugins['library'] = array(
+ *  '#title' => t('Library'),
+ *  '#attached' => array(
+ *    'js' => array(
+ *       $path . '/js/plugins/media.library.js',
+ *     ),
+ *   ),
+ *   '#settings' => array(
+ *     'viewMode' => 'thumbnails',
+ *     'getMediaUrl' => url('media/browser/list'),
+ *   ),
+ *   '#markup' => '<div> Library goes here</div>',
+ * );
+ *
+ * @param $plugin_name
+ *  The name of the plugin to view
+ *
+ * @param $params
+ *  An array of parameters which came in is $_GET['params'].
+ *  The expected parameters is still being defined.
+ *   - types: Array of media types to support
+ *   - multiselect: Boolean enabling or disabling multiselect
+ *
+ * @return
+ *  Renderable array.
+ */
+function hook_media_browser_plugin_view($plugin_name, $params) {
+
+}
 
 /**
  * Returns a list of plugins for the media browser.
  *
- * Media provides a CTools plugin API; this is one of those hooks. It should
- * return a nested array of plugin information, keyed by plugin name. Each
- * plugin info array may have the following keys:
- * - title (required): A name for the tab in the media browser.
- * - class (required): The class name of the handler. This class must
- *   implement a view() method, and may (should) extend the
- *   @link MediaBrowserPlugin MediaBrowserPlugin @endlink class.
- * - weight (optional): Integer to determine the tab order. Defaults to 0.
- * - access callback (optional): A callback for user access checks.
- * - access arguments (optional): An array of arguments for the user access
- *   check.
+ * Plugins are defined in a multi-dimensional associative
+ * array format with the following keys:
  *
- * Additional custom keys may be provided for use by the handler.
+ * - #weight (optional): Weight of the plugin in relation to other plugins
+ *  when being displayed, e.g. tabs in the browser.
  *
- * @see hook_media_browser_plugin_info_alter()
- * @see media_get_browser_plugin_info()
+ * @example
+ * <code>
+ * array(
+ *  'unique_plugin_name' => array(
+ *     '#weight' => 42,
+ *   ),
+ * );
+ * </code>
  */
 function hook_media_browser_plugin_info() {
-  $info['media_upload'] = array(
-    'title' => t('Upload'),
-    'class' => 'MediaBrowserUpload',
-    'weight' => -10,
-    'access callback' => 'user_access',
-    'access arguments' => array('create files'),
-  );
 
-  return $info;
 }
 
 /**
- * Alter the list of plugins for the media browser.
+ * Returns an array of operations which can be taken on media items.
  *
- * @param array &$info
- *   The associative array of media browser plugin definitions from
- *   hook_media_browser_plugin_info().
+ * This is used on the admin/content/media page so users can select multiple
+ * items and do something with them.
  *
- * @see hook_media_browser_plugin_info()
- * @see media_get_browser_plugin_info()
+ * The return format is an array or arrays with the following keys:
+ *  - label: The string to be shown to the user.
+ *  - callback (optional): A callback to be called when the media items are selected.
+ *     Media items will be passed in as an argument.
+ *  - redirect (optional): A path to redirect to.  %fids should be in the path
+ *     It will be replaced with the fids selected delimited by "+".
+ *     i.e. mymodule/%fids/something -> mymodule/1+3+2/something if media items
+ *     1, 3 and 2 were selected.
  */
-function hook_media_browser_plugin_info_alter(&$info) {
-  $info['media_upload']['title'] = t('Upload 2.0');
-  $info['media_upload']['class'] = 'MediaBrowserUploadImproved';
+function media_media_operations() {
+
 }
 
 /**
