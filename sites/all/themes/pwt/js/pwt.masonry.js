682f7b5b1a5cd39a24c75a1f49f67ae05a856b13
Kendrick <wallncrown@mac.com>
2012-11-23 13:05:30 -0800

    New Front Page = MODULE views_tree + PATCH for CSS/Masonry layout
    New layout in PW_Admin where a Taxonomy tree is built to show all the classified sections and create masonry blocks for the front page.

--- sites/all/themes/pwt/js/pwt.masonry.js
+++ sites/all/themes/pwt/js/pwt.masonry.js
@@ -5,6 +5,7 @@
 
 (function ($) {
   $(document).ready(function() {
+    // Classified Ads with Masonry
     $('.classified-ad-group-section > img').imagesLoaded(function() {
       $('.classified-ad-group-section').masonry({
         itemSelector : '.classified-ad-block',
@@ -12,6 +13,14 @@
         isFitWidth: true
       });
     });
+
+    // Classified Section Listings
+    $('.stem_class').masonry({
+      itemSelector : '.branch_class',
+      columnWidth : 285,
+      isFitWidth: true
+    });
+
   });
 }(jQuery));
 
