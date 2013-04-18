e7da4c68228c287abc197b7a4f27fc6f6dc136e0
Kendrick <wallncrown@mac.com>
2013-04-18 13:43:23 -0700

    Update layout of Area Phone Code block. - layout CSS - added JS script for toggle hide/show - added JS script for area phone .masonry()

--- sites/default/modules/pennywise/js/pw_admin.js
+++ sites/default/modules/pennywise/js/pw_admin.js
@@ -1,12 +1,11 @@
 (function($) {
 
-// Toggle For Hiding/Unhiding Ad Submission 'More' Data
 $(document).ready(function() {
   // jCarousel fixer.
   if($('#imageScroller').length > 0) {
     $('#imageScroller').jcarousel({ scroll :1, itemFallbackDimension:280 });
   }
-
+  // Toggle For Hiding/Unhiding Ad Submission 'More' Data
   // Hide all content that needs to hide.
   if ($('.ad-details-toggle-box-content').length > 0) {
     $('.ad-details-toggle-box-content').hide();
@@ -16,6 +15,19 @@ $(document).ready(function() {
   $('[id^=ad-details-toggle-box-switch-]').click(function() {
     $('#ad-details-'+($(this).attr('class'))).toggle('slow');
   });
+
+  //Area Phone Codes show-hide
+  $('.page-taxonomy-term #phone-calling-codes, #phone-calling-codes-title').hide();
+  $('#block-pennywise-pw-phone-areas .block-title').css('cursor', 'pointer').click(function() {
+    $('#phone-calling-codes, #phone-calling-codes-title').toggle();
+    // Do masonry on it.
+    $('#phone-calling-codes').masonry({
+      itemSelector : '.city-phone-group',
+      columnWidth : 200,
+    })
+    return false;
+  });
+
 })
 
 }(jQuery));
